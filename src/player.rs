fb44bcf85ab20230605b4cdd571ac6d13cbf6daa
Ben Whitley <dev@purplg.com>
2024-06-17 18:25:10 -0400

    Use entities to track positions

--- src/player.rs
+++ src/player.rs
@@ -7,34 +7,58 @@ pub(crate) struct Plugin;
 
 impl bevy::prelude::Plugin for Plugin {
     fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup);
+        app.add_systems(
+            Update,
+            update_position_system.run_if(on_event::<MumbleLinkEvent>()),
+        );
         app.add_systems(Update, position);
         app.add_systems(Update, save_pos_system);
     }
 }
 
+#[derive(Component)]
+struct Player;
+
+fn setup(mut commands: Commands) {
+    let mut entity = commands.spawn_empty();
+    entity.insert(Player);
+    entity.insert(TransformBundle::default());
+}
+
+fn update_position_system(
+    mut events: EventReader<MumbleLinkEvent>,
+    mut query: Query<&mut Transform, With<Player>>,
+) {
+    let mut transform = query.single_mut();
+    for event in events.read() {
+        if let MumbleLinkEvent::Data(data) = event {
+            transform.translation = Vec3 {
+                x: data.avatar.position[0],
+                y: data.avatar.position[1],
+                z: -data.avatar.position[2],
+            }
+        }
+    }
+}
+
 fn position(
     mut gizmos: Gizmos,
-    mumbledata: Res<MumbleData>,
-    query: Query<&Transform, With<SavedPosition>>,
+    player: Query<&Transform, With<Player>>,
+    saved: Query<&Transform, With<SavedPosition>>,
 ) {
     let position = Vec3::new(0., 120., 0.);
     gizmos.sphere(position, Quat::default(), 1.0, basic::RED);
 
-    if let Ok(saved_pos) = query.get_single() {
+    if let Ok(saved_pos) = saved.get_single() {
         let pos = saved_pos.translation;
         gizmos.sphere(pos, Quat::default(), 1.0, basic::FUCHSIA);
     }
 
-    if let Some(data) = &mumbledata.0 {
-        let player = Vec3::new(
-            data.avatar.position[0],
-            data.avatar.position[1],
-            -data.avatar.position[2],
-        );
-        gizmos.arrow(player, player + Vec3::X, basic::RED);
-        gizmos.arrow(player, player + Vec3::Y, basic::GREEN);
-        gizmos.arrow(player, player + Vec3::Z, basic::BLUE);
-    }
+    let player = player.single().translation;
+    gizmos.arrow(player, player + Vec3::X, basic::RED);
+    gizmos.arrow(player, player + Vec3::Y, basic::GREEN);
+    gizmos.arrow(player, player + Vec3::Z, basic::BLUE);
 }
 
 #[derive(Component)]
@@ -43,30 +67,18 @@ struct SavedPosition;
 fn save_pos_system(
     mut commands: Commands,
     mut events: EventReader<MumbleLinkEvent>,
-    mumbledata: Res<MumbleData>,
-    mut query: Query<&mut Transform, With<SavedPosition>>,
+    player: Query<&Transform, With<Player>>,
+    mut saved: Query<&mut Transform, (With<SavedPosition>, Without<Player>)>,
 ) {
     for event in events.read() {
         if let MumbleLinkEvent::Save = event {
-            if let Some(data) = &mumbledata.0 {
-                if let Ok(mut pos) = query.get_single_mut() {
-                    pos.translation = Vec3::new(
-                        data.avatar.position[0],
-                        data.avatar.position[1],
-                        -data.avatar.position[2],
-                    );
-                    println!("position updated");
-                } else {
-                    commands.spawn((
-                        SavedPosition,
-                        Transform::from_xyz(
-                            data.avatar.position[0],
-                            data.avatar.position[1],
-                            -data.avatar.position[2],
-                        ),
-                    ));
-                    println!("new position saved");
-                }
+            let player = player.single().translation;
+            if let Ok(mut pos) = saved.get_single_mut() {
+                pos.translation = player;
+                println!("position updated");
+            } else {
+                commands.spawn((SavedPosition, Transform::from_translation(player)));
+                println!("new position saved");
             }
         }
     }
